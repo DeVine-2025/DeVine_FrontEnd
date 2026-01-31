8edf904e4bf7091b41653d0d93ba6806eb7c79ae
달수 <emily4542@naver.com>
2026-01-31 21:35:44 +0900

    chore: SSO 콜백 페이지 export 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -31,3 +31,4 @@ export { default as ProjectSearchPage } from './search/project/project-search-pa
 export { default as SearchPage } from './search/search-page';
 
 export { default as SignupPage } from './signup/signup-page';
+export { default as SsoCallbackPage } from './auth/sso-callback-page';
