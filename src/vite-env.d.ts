53d27611dbf6baa1acc82bfe63f498b83c7e61c3
달수 <emily4542@naver.com>
2026-01-31 21:33:44 +0900

    chore: VITE_CLERK_PUBLISHABLE_KEY 타입 선언

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,3 +10,11 @@ declare module '*.svg' {
   const src: string;
   export default src;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_CLERK_PUBLISHABLE_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
