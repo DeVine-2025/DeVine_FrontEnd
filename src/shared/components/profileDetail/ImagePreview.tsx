28399389aa81bdf2864758fda1e25be3ff5ecfa8
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2026-02-05 22:41:24 +0900

    fix: 버셀 오류 해결

--- src/shared/components/profileDetail/ImagePreview.tsx
+++ src/shared/components/profileDetail/ImagePreview.tsx
@@ -1,5 +1,5 @@
 import ProfileDefaultIcon from '@assets/icons/profile-default.svg?react';
-import DummyImage from "@assets/images/dummyImage.jpg";
+// import DummyImage from "@assets/images/dummyImage.jpg";
 
 import {cn} from '@libs/cn';
 
@@ -12,7 +12,7 @@ const ImagePreview = ({isExist, className}: ImagePreviewProps) => {
   return (
     <div className={cn('bg-ui-100 shrink-0 border border-ui-200 overflow-hidden rounded-full w-24 h-24 flex-col-center', className)}>
       {isExist ?
-        <img alt="profile" src={DummyImage} className="w-full h-full object-cover"/>
+        <img alt="profile" className="w-full h-full object-cover"/>
         : <ProfileDefaultIcon className="text-[var(--ui-bg)] w-15 h-15 m-6"/>
       }
     </div>
