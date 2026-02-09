49486aa3fb653d4c55e799a18dc0e53bd5577f3c
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2026-02-09 18:20:57 +0900

    feat: 불필요 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useAuth } from '@clerk/clerk-react';
 import queryClient from '@libs/query-client';
 import { router } from '@routes/routers';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { useAuth } from '@clerk/clerk-react';
 import { useEffect } from 'react';
