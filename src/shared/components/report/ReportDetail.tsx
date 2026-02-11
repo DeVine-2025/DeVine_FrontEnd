0539c92d0f53ec39046d876833c878af25d2f524
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2026-02-11 23:55:30 +0900

    feat: 코드 분석 인사이트, 개선 가능 영역 작업 완료

--- src/shared/components/report/ReportDetail.tsx
+++ src/shared/components/report/ReportDetail.tsx
@@ -1,5 +1,7 @@
 import { ReportDetailContent, ReportMainContent } from '@apis/report/report';
 import ContentBox from '@components/report/ContentBox';
+import CheckGreenIcon from "@assets/icons/detail-page/check-green.svg?react";
+import CancelRedIcon from "@assets/icons/detail-page/cacanel-red.svg?react";
 
 type ReportDetailProps = {
   data: ReportDetailContent;
@@ -69,11 +71,25 @@ const ReportDetail = ({ data }: ReportDetailProps) => {
           <hr className="border-ui-100" />
           <div>
             <ContentBox>
-              <div>
-                <div>
-                  <p className="w-fit px-2 py-1 bg-positive-bg text-positive-text rounded-lg">이 프로젝트에서 구현한 것</p>
+              <div className="flex p-[3.2rem]">
+                <div className="flex-col gap-4 flex-1/3">
+                  <p className="w-fit px-2 py-1 bg-positive-bg text-positive-text rounded-lg font-semibold">이 프로젝트에서 구현한 것</p>
+
+                  <div className="flex-col gap-2">
+                    {data?.projectSummary.implemented.map((item) => (
+                      <p className="flex items-center gap-2 text-ui-900 text-base font-normal"><CheckGreenIcon className="w-5 h-5" />{item}</p>
+                    ))}
+                  </div>
                 </div>
-                <div></div>
+                <div className="flex-col gap-4 flex-1/3">
+                  <p className="w-fit px-2 py-1 bg-negative-bg text-negative-text rounded-lg font-semibold">이 프로젝트에서
+                    구현하지 않은 것</p>
+                  <div className="flex-col gap-2">
+                    {data?.projectSummary.notImplemented.map((item) => (
+                      <p className="flex items-center gap-2 text-ui-900 text-base font-normal"><CancelRedIcon className="w-5 h-5" />{item}</p>
+                    ))}
+                  </div>
+                </div>
               </div>
             </ContentBox>
           </div>
@@ -91,6 +107,30 @@ const ReportDetail = ({ data }: ReportDetailProps) => {
         <div className="flex-col gap-[1rem]">
           <p className={sectionTitleStyle}>개선 가능한 영역</p>
           <hr className="border-ui-100" />
+          <p className=" text-ui-1000 text-lg font-bold"></p>
+          {data?.improvements?.map((item) => (
+            <ContentBox>
+              <div className="p-[3rem] flex-col gap-10">
+                <p className="text-ui-1000 text-lg font-bold">{item.number + ' . ' +item?.title}</p>
+                <div className="flex">
+                  <div className="flex-1/3 flex-col gap-1">
+                    <p className="w-fit px-2 py-1  bg-ui-100  text-ui-700 text-xs rounded-lg font-semibold">현재 코드</p>
+                    <div>
+                      {item.currentState.map((description) => (<p className="text-ui-700 text-sm font-normal">• {description}</p>))}
+                    </div>
+
+                  </div>
+                  <div className="flex-1/3 flex-col gap-1">
+                  <p
+                      className="w-fit px-2 py-1 bg-positive-bg text-positive-text text-xs rounded-lg font-semibold">개선방향</p>
+                    {item.suggestions.map((description) => (<p className="text-ui-700 text-sm font-normal">• {description}</p>))}
+                  </div>
+                </div>
+                <p className="flex gap-4 items-center text-ui-400 text-xs font-semibold"><span
+                  className="text-primary text-xs font-bold">학습 키워드</span>{item?.keywords}</p>
+              </div>
+            </ContentBox>
+          ))}
         </div>
       </section>
 
@@ -98,6 +138,16 @@ const ReportDetail = ({ data }: ReportDetailProps) => {
         <div className="flex-col gap-[1rem]">
           <p className={sectionTitleStyle}>다음 프로젝트에서 시도해볼 만한 것</p>
           <hr className="border-ui-100" />
+          {data?.nextSteps.map((item) => (
+            <ContentBox>
+              <div className="p-[3rem] flex-col gap-[0.7rem]">
+                <p className="text-ui-1000 text-xl font-bold">{item.number + ' . ' + item.title}</p>
+                <p className="text-ui-400 text-base">{item.description}</p>
+                {/*<p className="text-ui-1000 text-sm font-medium"><span className="text-primary text-[10px] font-bold px-2 py-1 bg-indigo-600/10 rounded outline outline-1 outline-offset-[-1px] outline-indigo-600/20">추천 기술</span></p>*/}
+              </div>
+            </ContentBox>
+          ))}
+
         </div>
       </section>
     </div>
